4e374bd44ee6daa634db800819bf2204c54dccec
Paula Rivas <pcrivas@utpl.edu.ec>
2023-10-19 17:18:47 -0500

    primer commit

---
+++ pcrivas-Miprimerprograma/src/main/java/com/mycompany/pcrivas/miprimerprograma/PcrivasMiprimerprograma.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pcrivas.miprimerprograma;
+
+/**
+ *
+ * @author SALAI
+ */
+public class PcrivasMiprimerprograma {
+
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+}
