57331ad752cd6277420a48d21be5600cca803102
Paula Rivas <pcrivas@utpl.edu.ec>
2023-10-24 12:14:18 -0500

    PRIMER COMMIT

---
+++ PROYECTOS/PROYECTO 01/src/proyecto/pkg01/PROYECTO01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto.pkg01;
+
+/**
+ *
+ * @author Paula
+ */
+public class PROYECTO01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejemplo basico de JAVA versionado a través de Git manejando desde GitHub");
+    }
+    
+}
---
+++ PROYECTOS/PROYECTO 02/PROYECTO 02/src/proyecto/pkg02/PROYECTO02.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto.pkg02;
+
+/**
+ *
+ * @author Paula
+ */
+public class PROYECTO02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos basico de JAVA versinado a traves de Git manejado desde GitHub ademas de usar GitHubClassroom");
+    }
+    
+}
